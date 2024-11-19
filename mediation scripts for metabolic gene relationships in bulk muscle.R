d126428749e8f78d8adbdaefa55763c715b71455
Marcus Seldin <mseldin3@gmail.com>
2024-11-18 19:59:29 -0800

    Update mediation scripts for metabolic gene relationships in bulk muscle.R

--- mediation scripts for metabolic gene relationships in bulk muscle.R
+++ mediation scripts for metabolic gene relationships in bulk muscle.R
@@ -1,6 +1,5 @@
-setwd('G:/My Drive/lab files/Lauren Albrecht/muslce pfk story/mediation examples')
-load('G:/My Drive/lab files/sex-difference myokine study/github/raw files/GTEx NA included env.RData')
-human_pathways = read.delim('G:/My Drive/Datasets/Human/genome files/uniprot-human-genes and goterms mapping.tab')
+setwd('')
+load('GTEx NA included env.RData')
 library(WGCNA)
 library(ggplot2)
 library(pheatmap)
@@ -10,23 +9,14 @@ library(ggpubr)
 filtered_working = GTEx_subfiltered
 filtered_working$gene_tissue=NULL
 
-lauren_paths = read.csv('G:/My Drive/lab files/Lauren Albrecht/muslce pfk story/lauren curated pathways.csv')
-table(lauren_paths$Pathway)
-table(lauren_paths$Gene[lauren_paths$Pathway=='Lysosome'])
-
-
 filtered_working = GTEx_full
-table(colnames(filtered_working[grepl('ITIH5', colnames(filtered_working))]))
-
-gene_tissue_origin = 'PFKM_Muscle - Skeletal'
-covariate_gene_tissue = 'LAMP1_Muscle - Skeletal'
-pathway_term = 'PPP'
-pathway_tissue = 'Muscle - Skeletal'
+#table(colnames(filtered_working[grepl('ITIH5', colnames(filtered_working))]))
 
 
 run_full_adj = function(gene_tissue_origin, covariate_gene_tissue, pathway_term, pathway_tissue){
   
-  new_paths = lauren_paths[lauren_paths$Pathway==pathway_term,]
+  new_paths = as.data.frame(gene_list1)
+  colnames(new_paths) = 'Gene'
   new_paths$tissuepaste = paste0(pathway_tissue)
   new_paths$gene_tissue = paste0(new_paths$Gene, '_', new_paths$tissuepaste)
   #write.csv(new_paths, file = paste0('Pathway genes listed for ', pathway_tissue, ' ', pathway_term, '.csv'), row.names = F)
@@ -45,7 +35,7 @@ run_full_adj = function(gene_tissue_origin, covariate_gene_tissue, pathway_term,
   cor1 = bicorAndPvalue(combined_key$`filtered_working[, colnames(filtered_working) %in% gene_tissue_origin]`, gg2)
   resids = summary(lm(combined_key$`filtered_working[, colnames(filtered_working) %in% gene_tissue_origin]` ~ combined_key$`filtered_working[, colnames(filtered_working) %in% covariate_gene_tissue]`))$residuals
   
-  
+  library(viridis)
   cor2 = bicorAndPvalue(resids, gg2)
   heat_set = as.data.frame(rbind(cor1$bicor, cor2$bicor))
   heat_setp = as.data.frame(rbind(cor1$p, cor2$p))
@@ -58,7 +48,9 @@ run_full_adj = function(gene_tissue_origin, covariate_gene_tissue, pathway_term,
   pval_settings = ifelse(heat_setp<1e-6, '***', pval_settings)
   fontsize_number = 20 ## increase label size
   pdf(file = paste0('heatmap of ', gene_tissue_origin, ' correlated with ', pathway_tissue, ' ', pathway_term, ' ', covariate_gene_tissue, '-adjusted.pdf'))
-  pp1 = pheatmap(heat_set, cluster_rows = F, cluster_cols = F, display_numbers = pval_settings, fontsize_number = 30, color = colorRampPalette(c("darkorchid", "darkorange", "white"))(100))
+  #pp1 = pheatmap(heat_set, cluster_rows = F, cluster_cols = F, display_numbers = pval_settings, fontsize_number = 30, color = colorRampPalette(c("darkorchid", "darkorange", "white"))(100))
+  pp1 = pheatmap(heat_set, cluster_rows = F, cluster_cols = F, display_numbers = pval_settings, number_color = "white",fontsize_number = 30, color = viridis(n = 256, alpha = 1, option = "magma"))
+  
   print(pp1)
   dev.off()
   
@@ -88,7 +80,23 @@ run_full_adj = function(gene_tissue_origin, covariate_gene_tissue, pathway_term,
   dev.off()
 }
 
+gene_list1 = c('ALDOA', 'ALDOC','TKT', 'G6PDH', 'H6PD', 'PGD', 'PGLS', 'PGM1', 'PRPS1','RPE', 'RPIA', 'TALDO')
+pathway_term = 'ALL PPP'
+
+run_full_adj('PFKM_Muscle - Skeletal', 'LAMP1_Muscle - Skeletal', 'ALL PPP', 'Muscle - Skeletal')
+run_full_adj('PFKM_Muscle - Skeletal', 'PRMT1_Muscle - Skeletal', 'ALL PPP', 'Muscle - Skeletal')
+
+gene_list1 = c('H6PD', 'PGD', 'PGLS', 'PGM1', 'PRPS1','RPE', 'RPIA', 'TALDO')
+pathway_term = 'Cherry picked set PPP'
+
+run_full_adj('PFKM_Muscle - Skeletal', 'LAMP1_Muscle - Skeletal', 'Cherry picked set PPP', 'Muscle - Skeletal')
+run_full_adj('PFKM_Muscle - Skeletal', 'PRMT1_Muscle - Skeletal', 'Cherry picked set PPP', 'Muscle - Skeletal')
+
+gene_list1 = c('ALDH1B1', 'PGK1', 'GPI', 'ACSS2', 'HK2','PGM1', 'PGAM2', 'ENO3', 'GALM', 'HK1', 'FBP2')
+
+run_full_adj('PFKM_Muscle - Skeletal', 'LAMP1_Muscle - Skeletal', 'Glycolysis', 'Muscle - Skeletal')
+run_full_adj('PFKM_Muscle - Skeletal', 'PRMT1_Muscle - Skeletal', 'Glycolysis', 'Muscle - Skeletal')
 
 
-run_full_adj('PFKM_Muscle - Skeletal', 'LAMP1_Muscle - Skeletal', 'PPP', 'Muscle - Skeletal')
-run_full_adj('PFKM_Muscle - Skeletal', 'LAMP2_Muscle - Skeletal', 'PPP', 'Muscle - Skeletal')
+
+
